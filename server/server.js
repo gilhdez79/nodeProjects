fb6fd8cb44c8b04808df10ce767e607ddf15d77e
Gilberto Hernandez <gilhdez79@yahoo.com.mx>
2020-06-26 22:51:53 -0500

    connect disconect

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 var app = express();
+var server = http.createServer(app);
 const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
+var io = socketIO(server);
 
+io.on('connection',(socket)=>{
+  console.log('New user connected');
+});
 
 app.use(express.static(publicPath));
 console.log(publicPath);
 
 
-app.listen(3000, ()=>{
+server.listen(port, ()=>{
   console.log('Server is up on port 3000');
 
 });
